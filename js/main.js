2296ec98fc23450068837e8580b16b8e00122e7d
zaharia191 <zahariavaleria130@gmail.com>
2025-02-13 16:32:48 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+AOS.init({
+    duration: 800,
+    once: true
+});
+
+new SimpleLightbox({elements: ".galerie a"});
+function mobileMenu() {
+    var x = document.getElementsByTagName("nav")[0];
+    if (x.className === "") {
+        x.className += "mobile";
+    } else {
+        x.className = "";
+    }
+
+}
